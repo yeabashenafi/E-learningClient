6ceaa866ef1ad6d615fc9a4161ced7f94ea24bfe
EyobAshenaki <ethioeyoba@gmail.com>
2022-05-08 16:43:37 +0300

    fix(layout): patch default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,25 +1,12 @@
 <template>
-  <v-app>
-    <app-header />
-    <app-side-bar />
-    <v-main>
-      <v-container>
-        <Nuxt />
-      </v-container>
-    </v-main>
-  </v-app>
+  <v-container class="main-container">
+    <Nuxt />
+  </v-container>
 </template>
 
 <script>
-import AppHeader from '@/components/AppHeader.vue'
-import AppSideBar from '@/components/AppSideBar.vue'
 export default {
   name: 'DefaultLayout',
-  components: { AppHeader, AppSideBar },
-  // comments: {
-  //   AppHeader,
-  //   AppSideBar
-  // },
   data() {
     return {}
   },
